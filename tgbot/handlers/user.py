fa729f906347e0731acba5342d14d297850f5eb9
rakhmanov-aybulat <rakhmanov.aybulat.21@gmail.com>
2022-05-27 09:43:36 +0300

    feat: create a command to change the school grade

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,23 +1,41 @@
-from aiogram import Dispatcher
-from aiogram.types import Message
+from aiogram import Dispatcher, types
+from aiogram.dispatcher import filters
 
 from psycopg2.errors import UniqueViolation
 
 from tgbot.services.repository import Repo
 
 
-async def user_start(m: Message, repo: Repo):
-    await m.reply("Hello, user!")
-
+async def user_start(m: types.Message, repo: Repo):
     chat_id = m.from_user.id
     first_name = m.from_user.first_name
     last_name = m.from_user.last_name
     user_name = m.from_user.username
     
+    await m.answer(f'Привет, {first_name}')
+
     try:
         await repo.add_user(chat_id, first_name, last_name, user_name)
     except UniqueViolation:
-        await repo.update_user(chat_id, first_name, last_name, user_name)
-        
+        await repo.update_user(chat_id, first_name, last_name, user_name) 
+
+    if await repo.has_user_grade(chat_id):
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(types.KeyboardButton(text='Сколько минут до звонка?'))
+        await m.answer('Спроси меня: "Сколько минут до звонка?"', reply_markup=keyboard)
+    else:
+        await user_change_grade(m, repo)
+
+
+async def user_change_grade(m: types.Message, repo: Repo):
+    keyboard = types.InlineKeyboardMarkup()
+
+    grade_numbers = await repo.get_grade_numbers_list()
+    for g in grade_numbers:
+        keyboard.insert(types.InlineKeyboardButton(text=str(g[0]), callback_data=f'grade_number:{g[0]}'))
+
+    await m.answer('В каком классе ты учишься?', reply_markup=keyboard)
+
 def register_user(dp: Dispatcher):
-    dp.register_message_handler(user_start, commands=["start"], state="*")
+    dp.register_message_handler(user_start, commands=['start'], state='*')
+    dp.register_message_handler(user_change_grade, commands=['changegrade'], state='*')
--- tgbot/services/repository.py
+++ tgbot/services/repository.py
@@ -30,11 +30,44 @@ class Repo:
 
         return
 
-    async def list_users(self) -> List[int]:
-        """List all bot users"""
-        return [
-            # row[0]
-            # async for row in self.conn.execute(
-            #     "select userid from tg_users",
-            # )
-        ]
+    async def get_grade_numbers_list(self) -> List[int]:
+        with self.conn as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                    SELECT DISTINCT gradenumber 
+                    FROM grades 
+                    ORDER BY gradenumber ASC;
+                    ''')
+
+                return cursor.fetchall()
+
+    async def get_grade_letter_list(self, grade_number: int) -> List[str]:
+        with self.conn as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                    SELECT gradeletter 
+                    FROM grades
+                    WHERE gradenumber = %s;
+                    ''', (grade_number,))
+
+                return cursor.fetchall()
+
+    async def has_user_grade(self, chat_id: int) -> bool:
+        with self.conn as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('''
+                    SELECT gradenumber
+                    FROM users
+                    WHERE chatid = %s;
+                    ''', (chat_id,))
+                return cursor.fetchone()[0] is not None
+
+    async def add_user_grade(self, chat_id, grade_number, grade_letter) -> None:
+        with self.conn as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    '''UPDATE users SET 
+                        gradenumber = %s,
+                        gradeletter = %s
+                        WHERE chatid = %s;''',
+                    (grade_number, grade_letter, chat_id))
